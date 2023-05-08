630332b418a9d1aa4a5ffe252667910f84fc6042
Dusia <Dusia@DESKTOP-J5Q6K5G>
2023-05-08 18:40:33 +0200

    add UI

--- src/app/components/invoices/invoices.component.spec.ts
+++ src/app/components/invoices/invoices.component.spec.ts
@@ -14,6 +14,7 @@ describe('InvoicesComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+  //no tetss
 
   it('should create', () => {
     expect(component).toBeTruthy();
